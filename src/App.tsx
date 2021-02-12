593137427bb3192c87e27686dc40ccece44a5895
Samarth Kulshrestha <samarthkulshresth@gmail.com>
2021-02-12 19:53:56 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,140 @@
+import React, { useState, useCallback, useRef } from "react";
+import "./App.css";
+import produce from "immer";
+
+const numRows = 100;
+const numCols = 100;
+
+const operations = [
+  [0, 1],
+  [0, -1],
+  [1, -1],
+  [-1, 1],
+  [1, 1],
+  [-1, -1],
+  [1, 0],
+  [-1, 0],
+];
+
+const generateEmptyGrid = () => {
+  const rows = [];
+  for (let i = 0; i < numRows; i++) {
+    rows.push(Array.from(Array(numCols), () => 0));
+  }
+
+  return rows;
+};
+
+const App: React.FC = () => {
+  const [grid, setGrid] = useState(() => {
+    return generateEmptyGrid();
+  });
+
+  const [running, setRunning] = useState(false);
+
+  const runningRef = useRef(running);
+  runningRef.current = running;
+
+  const runSimulation = useCallback(() => {
+    if (!runningRef.current) {
+      return;
+    }
+
+    setGrid((g) => {
+      return produce(g, (gridCopy) => {
+        for (let i = 0; i < numRows; i++) {
+          for (let k = 0; k < numCols; k++) {
+            let neighbors = 0;
+            operations.forEach(([x, y]) => {
+              const newI = i + x;
+              const newK = k + y;
+              if (newI >= 0 && newI < numRows && newK >= 0 && newK < numCols) {
+                neighbors += g[newI][newK];
+              }
+            });
+
+            if (neighbors < 2 || neighbors > 3) {
+              gridCopy[i][k] = 0;
+            } else if (g[i][k] === 0 && neighbors === 3) {
+              gridCopy[i][k] = 1;
+            }
+          }
+        }
+      });
+    });
+
+    setTimeout(runSimulation, 1);
+  }, []);
+
+  return (
+    <div id="cont">
+      <a
+        href="https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <h1 id="heading">Conway's Game of Life</h1>
+      </a>
+      <p>Click on the above link to know more. (its interesting, i promise)</p>
+      <div id="btnCont">
+        <button
+          className="button"
+          onClick={() => {
+            setRunning(!running);
+            if (!running) {
+              runningRef.current = true;
+              runSimulation();
+            }
+          }}
+        >
+          {running ? "stop" : "start"}
+        </button>
+        <button
+          className="button"
+          onClick={() => {
+            const rows = [];
+            for (let i = 0; i < numRows; i++) {
+              rows.push(
+                Array.from(Array(numCols), () => (Math.random() > 0.7 ? 1 : 0))
+              );
+            }
+
+            setGrid(rows);
+          }}
+        >
+          random
+        </button>
+        <button
+          className="button"
+          onClick={() => {
+            setGrid(generateEmptyGrid());
+          }}
+        >
+          clear
+        </button>
+      </div>
+      {/* <hr /> */}
+      <div id="canvas">
+        {grid.map((rows, i) =>
+          rows.map((col, k) => (
+            <div
+              className="cell"
+              key={`${i}-${k}`}
+              onClick={() => {
+                const newGrid = produce(grid, (gridCopy) => {
+                  gridCopy[i][k] = grid[i][k] ? 0 : 1;
+                });
+                setGrid(newGrid);
+              }}
+              style={{
+                backgroundColor: grid[i][k] ? "green" : "black",
+              }}
+            />
+          ))
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default App;
